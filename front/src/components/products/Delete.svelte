6d9d61ac0e208f367fe69b1647643ed76d8dcae1
LucasLongo <lucaslongo2030@gmail.com>
2026-05-29 19:58:15 -0300

    creamos_2_nuevas_categorias

---
+++ front/src/components/products/Delete.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    let { productModel } = $props();
+</script>
+
+{#if $productModel.deleteDialog}
+<div class="w-full h-full fixed top-0 left-0 flex items-center justify-center backdrop-blur-xl">
+    <div class="bg-white p-4 rounded-md w-80">
+        <h2 class="text-lg font-bold">Eliminar Producto</h2>
+        <p>¿Seguro que desea eliminar este producto?</p>
+
+        <div class="flex justify-end gap-2 mt-3">
+            <button class="bg-gray-400 text-white px-4 py-2 rounded-md"
+                on:click={() => productModel.deleteDialog.set(false)}>
+                Cancelar
+            </button>
+
+            <button class="bg-red-500 text-white px-4 py-2 rounded-md"
+                on:click={() => productModel.deleteProductAction($productModel.selectedProduct.id)}>
+                Eliminar
+            </button>
+        </div>
+    </div>
+</div>
+{/if}
---
+++ front/src/components/products/Tabla.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+    import Delete from "./Delete.svelte";
+    import Edit from "./Edit.svelte";
+    import Create from "./Create.svelte";
+    import { productModel } from "./products.svelte";
+    import { onMount } from "svelte";
+
+    onMount(async () => {
+        await productModel.getProducts();
+    });
+</script>
+
+<Delete {productModel} />
+<Edit {productModel} />
+<Create {productModel} />
+
+<div class="w-full flex justify-end mb-4">
+    <button class="bg-gray-800 text-xl text-white px-2 py-1 rounded-md border border-white" onclick={() => productModel.showCreateModal()}>
+        Nuevo Producto
+    </button>
+</div>
+
+<table class="flex-1 w-full">
+    <thead>
+        <tr>
+            <th class="bg-gray-800 text-white text-left p-2">Nombre</th>
+            <th class="bg-gray-800 text-white p-2">Acciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        {#each productModel.products as product}
+            <tr class="odd:bg-gray-100 dark:odd:bg-gray-700">
+                <td class="px-2 py-1">{product.name}</td>
+                <td class="px-2 py-1">
+                    <div class="flex justify-center gap-2">
+                        <button
+                            onclick={() => productModel.showEditModal(product)}
+                            aria-label="Editar"
+                            class="bg-gray-800 text-white px-4 rounded-md"
+                        >
+                            Editar
+                        </button>
+                        <button
+                            onclick={() => productModel.showDeleteModal(product)}
+                            aria-label="Eliminar"
+                            class="bg-red-500 text-white px-4 rounded-md"
+                        >
+                            Eliminar
+                        </button>
+                    </div>
+                </td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
